eeeabb7ab76e9549e0b8ab04e5e9d787dafc2269
Adam Makuchowski <maku971@gmail.com>
2023-07-31 21:04:47 +0200

    init joi body validation

--- src/api/controllers/productsController.ts
+++ src/api/controllers/productsController.ts
@@ -1,25 +1,20 @@
 import {Request, Response} from 'express'
 import ProductModel from '../../db/models/productModel'
 import {Product} from '../../interfaces/RequestTypes'
+import Joi from 'joi'
 
 const products = [1, 2, 3]
 
-export const createProduct = async (req: Request<any, any, Product>, res: Response): Promise<void> => {
+export const createProduct = async (req: Request, res: Response): Promise<void> => {
   const product = req.body
 
-  if (!product) {
-    res.json({
-      message: 'Invalid body'
-    })
-
-    return
-  }
-
   try {
-    const newProduct = new ProductModel(product)
-    const savedProduct = await newProduct.save()
+    // const newProduct = new ProductModel(product)
+    // const savedProduct = await newProduct.save()
 
-    res.json(savedProduct)
+    // res.json(savedProduct)
+
+    res.json(req.body)
   } catch (error: any) {
     console.error('Error when creating a new product:', error.message);
   }
