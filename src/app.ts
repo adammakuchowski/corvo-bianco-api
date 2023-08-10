146e4fe8156a47da5a4d8ecf0a3ab96f8cd8c1a2
Adam Makuchowski <maku971@gmail.com>
2023-08-10 06:19:14 +0200

    update initialize app

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,36 @@
-import express from 'express'
+import express, {Application} from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 import errorHandler from './middlewares/errorHandler'
-import {connectDB} from './db/db'
 import corsOptions from './corsConfig'
 import defaultServerRouter from './api/routes/defaultServerRouter'
 import productsRouter from './api/routes/productsRouter'
 import ordersRouter from './api/routes/ordersRouter'
 
-const app = express()
-connectDB()
+const setupMiddlewares = (app: Application) => {
+  app.use(morgan('dev'))
+  app.use(express.json())
+  app.use(cors(corsOptions))
+  // TODO: init helmet 
+}
 
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(cors(corsOptions))
-// TODO: init helmet 
-
-app.use('/', defaultServerRouter)
-app.use('/products', productsRouter)
-app.use('/orders', ordersRouter)
-
+const setupErrorHandling = (app: Application) => {
 // TODO: not found handler
 app.use(errorHandler)
+}
 
-export default app
+const createApp = () => {
+  const app = express()
+  
+  setupMiddlewares(app)
+
+  app.use('/', defaultServerRouter)
+  app.use('/products', productsRouter)
+  app.use('/orders', ordersRouter)
+
+  setupErrorHandling(app)
+
+  return app
+}
+
+export default createApp
