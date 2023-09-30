4c2afc9c562e707b81fd2f0835e6bd46bb16144d
Adam Makuchowski <maku971@gmail.com>
2023-09-30 11:11:08 +0200

    update mongoose models

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,13 +1,13 @@
 import mongoose, {ConnectOptions} from 'mongoose'
+
 import {logger} from '../app'
 import appConfig from '../configs/appConfig'
 
-const {database: {host, port, name}} = appConfig 
-const DB_URL = `mongodb://${host}:${port}/${name}`
+const {database: {host, port, name, mongoUrl}} = appConfig 
+const DB_URL = mongoUrl || `mongodb://${host}:${port}/${name}`
 
 export const connectDB = async (): Promise<void> => {
   const dbOptions: ConnectOptions = {}
-
   try {
     await mongoose.connect(DB_URL)
     logger.info('Connected to MongoDB')
